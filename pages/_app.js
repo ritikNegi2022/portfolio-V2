cd978d6ab13c610a1e50ac4cc398f9d80b151d45
ritikNegi2022 <negiritik2022@gmail.com>
2023-01-20 21:45:09 +0530

    ''

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import Head from "next/head";
-
+import Images from '../ImageProvider/Images';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,6 +10,12 @@ export default function App({ Component, pageProps }) {
         <meta
           name="description"
           content="This is a project website to practice on Next.js"
+        />
+        {/* <link rel="icon" type="image/ico" sizes="32x32" href="../public/favicon.ico"></link> */}
+        <link
+          rel="icon"
+          type="images/x-icon"
+          href={Images.icon}
         />
       </Head>
       <Component {...pageProps} />
